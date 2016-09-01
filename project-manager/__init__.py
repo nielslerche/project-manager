feac56b0385cbe361c946f81ea6b4b5ff0fa73f5
Niels Lerche Sørensen <nielslerches@gmail.com>
2016-09-01 10:10:37 +0200

    First commit

---
+++ project-manager/__init__.py
@@ -0,0 +1,7 @@
+# project-manager/__init__.py
+
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
